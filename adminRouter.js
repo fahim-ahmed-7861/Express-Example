e12d3220f4336b562324a97ce484ebbda0f64186
fahim-ahmed-7861 <fahimahmedshojib109@gmai.com>
2021-05-20 14:08:23 +0600

    done

--- adminRouter.js
+++ adminRouter.js
@@ -4,6 +4,36 @@ const express = require("express");
 
 const adminRouter = express.Router();
 
+adminRouter
+     .route('/:id')
+     .all((req,res,next)=>
+     {
+         req.id = req.params.id;
+
+         req.name = 'SHOJIB'
+
+         next();
+       
+     })
+     .get((req,res)=>
+     {
+        res.send(`${req.method} ${req.id}`);
+     })
+     .put((req,res)=>
+     {
+        res.send(`${req.method} ${req.id}`);
+     })
+     .post((req,res)=>
+     {
+        res.send(`${req.method} ${req.id}`);
+     })
+     .delete((req,res)=>
+     {
+        res.send(`${req.method} ${req.id} ${req.name}`);
+     })
+
+     
+
 adminRouter.get('/',(req,res)=>
 {
     res.send('Home Admin');
--- publicRouter.js
+++ publicRouter.js
@@ -4,9 +4,34 @@ const express = require("express");
 
 const publicRouter = express.Router();
 
+
+const log=(req,res,next)=>
+{
+    console.log('I am logging something');
+
+    next();
+
+
+}
+publicRouter.all('*',log);
+
 publicRouter.get('/',(req,res)=>
 {
     res.send('public Admin');
+})
+
+publicRouter.param('user',(req,res,next,id)=>
+{
+    req.user = id === '1' ? 'Admin' : 'Anonymous';
+
+    next();
+
+
+})
+
+publicRouter.get('/:user',(req,res)=>
+{
+    res.send(`Hello ${req.user}`)
 })
 
 publicRouter.get('/about',(req,res)=>{
